7e6b42c283f1df49f649ec52c783c59e1507fd09
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-13 09:35:06 +0530

    Simple Input command

---
+++ Input.py
@@ -0,0 +1,5 @@
+a=int(input("Enter 1st Name:"))
+b=int(input("Enter 2nd Number"))
+ 
+c=a+b
+print(c)
---
+++ Input1.py
@@ -0,0 +1,5 @@
+a=input("Enter the name:")
+b=input("Enter your favourite color:")
+
+print("Hello,",a,"!")
+print("Your favourite color is",b)
