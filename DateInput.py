8ca9655abc2b5ba04718cb86518f12526cae4455
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-13 09:41:29 +0530

    Print without space

--- DateInput.py
+++ DateInput.py
@@ -1,3 +1,5 @@
+#Python generally print space when we use comma in print fumction
+#To avoid this we can use "+" symbol
 dd=input("Enter the Date:")
 mm=input("Enter the Month:")
 yy=input("Enter the year:")
