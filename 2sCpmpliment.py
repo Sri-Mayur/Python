7736f15540b318a88e8f3a29b02238774505e4df
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-18 11:19:46 +0530

    18 Sept 2023

---
+++ 2sCpmpliment.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number:"))
+
+
+print("After inverting the given number by 1 bit:")
+
+print(~a)
---
+++ BitwiseAND.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+
+print("After implementing A AND B (A&B):")
+print(a&b)
