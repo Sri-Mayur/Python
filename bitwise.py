c7222a1672c10f39666dd5d214b2fc05840e48f8
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-18 11:15:35 +0530

    Understanding bitwise shifts

--- bitwise.py
+++ bitwise.py
@@ -1,5 +1,8 @@
 a=int(input("Enter the number:"))
 
 
-print("After shifting the given number by 1 bit:")
+print("After shifting the given number by 1 bit right:")
 print(a>>1)
+
+print("After shifting the given number by 1 bit left:")
+print(a<<1)
