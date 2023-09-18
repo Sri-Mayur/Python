7adc131c292d53c64d752cb120e59828b702a6d5
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-18 11:33:12 +0530

    18 sept 2023

---
+++ AssingnmentOperator.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+
+print("After adding 1 in a:")
+a+=1
+print(a)
+b+=2
+print("After addding 2 in b:")
+print(b)
+
+print("3rd Power of b")
+print(b**3)
+
+print("A is equal to B?")
+print(a!=b)
