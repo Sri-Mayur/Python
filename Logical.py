6e2e31ece9a6e578353e7b9d6fdfe41893fb3fc8
Sri-Mayur <mayursrivastava877@gmail.com>
2023-09-20 10:00:38 +0530

    Logical Practice

---
+++ Logical.py
@@ -0,0 +1,16 @@
+print(12 or 10)
+print(12 and 10)
+print(0 or 12)
+print(3*2**2+5+7%3)
+print(5*4%3-4+3)
+print(4//2+9//3 and 4%2*5)
+print(12|10)
+print(12 & 10) #XOR
+print(0 ^ 12) 
+a=3
+a&=True #a = a & 1
+print(a)
+
+a|=False
+print(a)
+
