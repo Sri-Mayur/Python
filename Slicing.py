157554431e170c323709fe13271eb105c8775cb6
Sri-Mayur <mayursrivastava877@gmail.com>
2023-10-31 11:40:33 +0530

    .

---
+++ Slicing.py
@@ -0,0 +1,7 @@
+l1=[1,2,3,4,5,6,7,8,9,10]
+
+print(l1[1:3])
+
+#print even indexes
+
+print(l1[::-1])
---
+++ tuple.py
@@ -0,0 +1,17 @@
+tulp1=(1,2,32,4,5,6)
+tulp2=(1)
+tulp3=(1,)
+
+#tulp1[2]=4
+#Error
+#Tuple is immutable
+
+
+
+print(sorted(tulp1, reverse=True))
+
+print(tulp1)
+
+print(type(tulp1))
+print(type(tulp2))
+print(type(tulp3))
