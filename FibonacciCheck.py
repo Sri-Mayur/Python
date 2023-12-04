820ddaffa7d063257eb438e35885b345c159021e
Sri-Mayur <mayursrivastava877@gmail.com>
2023-12-04 11:16:55 +0530

    4 December, 2022

--- FibonacciCheck.py
+++ FibonacciCheck.py
@@ -1,21 +1,32 @@
 # Write a python function check whether a number belongs to a fibonacci series or not.
 
-n = int (input("Enter the number: "))
+def fib(n):
 
-a, b = 0, 1
+    a=0
+    b=1
 
-while(a <= n):
-    if a == n:
-         result = True
-    a, b = b, a + b
+    l = [a, b]
 
-else:
-    reselt = False
+    for i in range (n+1):
+        c = a+b
+        a = b
+        b = c
 
+        l.append(c)
+
+    if(n in l):
+        return True
+
+    else:
+        return False
+    
+
+
+number = int(input("Enter the number: "))
+
+result = fib(number)
 
 if(result):
-    print("Fibonacci")
-
+    print("Fibonacci Member")
 else:
-    print("Not Fibonacci")
-
+    print("Not Fibonacci Member")
